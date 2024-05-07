c2de46fb491fba78c105a66c310865c9074f345b
Douglas Queiroz <douglas.queiroz@sptech.school>
2024-05-07 09:49:54 -0300

    @DouglasSDQ - subindo pagina NotFound

--- frontend/src/pages/NotFound/NotFound.jsx
+++ frontend/src/pages/NotFound/NotFound.jsx
@@ -1,8 +1,24 @@
+import React from "react";
+import style from "./NotFound.module.css"
+import Logo from '../../utils/assets/logo-scale0.svg'
+
+
 function NotFound() {
   return (
-    <div>
-      <h1>404</h1>
-      <p>Page not found</p>
+    <div className={style.divTodo}>
+      <div className={style.divConteudo}>
+
+        <div className={style.divImagem}>
+          <img className={style.imagem} src={Logo} alt=""/>
+          <div>404 Not Found</div>
+        </div>
+
+        <div className={style.textoInfomarivo}>
+          Não foi possivel encontrar o que você está procurando!
+        </div>
+
+
+      </div>
     </div>
   );
 }
