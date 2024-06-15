06ad37a9dca3b8209c2c7d4b0414c017b5648994
Gabriella <gabriellaperniconi@gmail.com>
2024-06-15 15:46:34 -0300

    Setando nome da barbearia na header

--- frontend/src/components/HeaderUsuario/HeaderUsuario.jsx
+++ frontend/src/components/HeaderUsuario/HeaderUsuario.jsx
@@ -1,37 +1,31 @@
 import iconeSair from '../../utils/assets/IconsHeaderUsuario/Icone Sair.svg'
-import exemploImg from '../../utils/assets/IconsHeaderUsuario/exemplo.jpeg'
 import iconEditar from '../../utils/assets/IconsHeaderUsuario/IconEditar.svg'
 import editFoto from '../../utils/assets/IconsHeaderUsuario/photo-edit_svgrepo.com.png'
 import { Button, TextField } from '@mui/material'
 import React, { useState, useEffect } from 'react'
 import api from '../../api'
-import utils from "../../utils/globals";
+import utils from "../../utils/globals"
 import styles from './HeaderUsuario.module.css'
+import { useNavigate } from 'react-router-dom'
 
-
-function cadastroBarbearia() {
-    window.location = '/cadastro-barbearia'
-}
-
-function abrirPerfilBarbeiro() {
-    window.location = '/meus-cortes'
-}
-
-function abrirPerfilBarbearia() {
-    window.location = '/funcionarios'
-}
-
-function paginaLogin() {
-    window.location = '/login'
-    sessionStorage.clear()
-}
-
-function HeaderUsuario(props) {
+function HeaderUsuario() {
+    const navigate = useNavigate()
     const [isAuth, setIsAuth] = useState(false)
     const [isModalOpen, setIsModalOpen] = useState(false)
-    const [userInfo, setUserInfo] = useState({ nome: '', celular: '', email: '', logradouro: '', numero: '', cidade: '', estado: '', cep: '' })
+    const [userInfo, setUserInfo] = useState({
+        nome: '',
+        celular: '',
+        email: '',
+        logradouro: '',
+        numero: '',
+        cidade: '',
+        estado: '',
+        cep: '',
+        imgPerfil: '',
+        nomeNegocio: ''
+    })
     const token = JSON.parse(sessionStorage.getItem('user'))
-    const nomeBarbearia = JSON.parse(sessionStorage.getItem('user')).nomeBarbearia
+    const nomeNegocio = sessionStorage.getItem('nomeNegocio')
 
     useEffect(() => {
         const validarTipoUsuario = async () => {
@@ -56,27 +50,6 @@ function HeaderUsuario(props) {
         validarTipoUsuario()
     }, [token])
 
-    function enviarAtualizacaoUsuario(email, nome, celular) {
-        const enviarAtualizacaoUsuario = async () => {
-            try {
-                const response = await api.put('usuarios/editar-perfil', {
-                    nome: nome,
-                    email: email,
-                    celular: celular
-                }, {
-                    headers: {
-                        Authorization: token
-                    }
-                });
-                console.log(response.data);
-                closeModal(true);
-            } catch (error) {
-                console.error('Erro ao enviar a atualização do funcionário', error);
-            }
-        };
-        enviarAtualizacaoUsuario();
-    }
-
     useEffect(() => {
         const fetchUserInfo = async () => {
             try {
@@ -85,27 +58,50 @@ function HeaderUsuario(props) {
                         Authorization: token
                     }
                 })
-                const { nome, celular, email, logradouro, numero, cidade, estado, cep } = response.data
-                console.log(response.data)
-                setUserInfo({ nome, celular, email, logradouro, numero, cidade, estado, cep })
+                const { nome, celular, email, logradouro, numero, cidade, estado, cep, imgPerfil } = response.data
+                setUserInfo({ nome, celular, email, logradouro, numero, cidade, estado, cep, imgPerfil })
             } catch (error) {
                 console.error('Erro ao receber as informações do funcionário', error)
             }
         }
 
+        const fetchBarbeariaInfo = async () => {
+            try {
+                const response = await api.get('/barbearias/perfil', {
+                    headers: {
+                        Authorization: token
+                    }
+                })
+                const { nomeNegocio } = response.data
+                sessionStorage.setItem('nomeNegocio', nomeNegocio)
+            } catch (error) {
+                console.error('Erro ao receber as informações da barbearia', error)
+            }
+        }
+
+        fetchBarbeariaInfo()
         fetchUserInfo()
     }, [token])
 
-    const openModal = () => {
-        setIsModalOpen(true)
-    }
-
-    const closeModal = () => {
-        setIsModalOpen(false)
-    }
-
     const handleLogout = () => {
-        utils.logout();
+        utils.logout()
+    }
+
+    const cadastroBarbearia = () => {
+        navigate('/cadastro-barbearia')
+    }
+
+    const abrirPerfilBarbeiro = () => {
+        navigate('/meus-cortes')
+    }
+
+    const abrirPerfilBarbearia = () => {
+        navigate('/minha-agenda')
+    }
+
+    const paginaLogin = () => {
+        window.location = '/login'
+        sessionStorage.clear()
     }
 
     const [values, setValues] = useState({
@@ -128,7 +124,8 @@ function HeaderUsuario(props) {
             logradouro: '',
             numero: '',
             cidade: '',
-            cep: ''
+            cep: '',
+            imgPerfil: ''
         })
     }
 
@@ -136,200 +133,152 @@ function HeaderUsuario(props) {
         setValues({ ...values, [prop]: event.target.value })
     }
 
+    const enviarAtualizacaoUsuario = async () => {
+        try {
+            const response = await api.put('usuarios/editar-perfil', {
+                nome: values.nome,
+                email: values.email,
+                celular: values.celular
+            }, {
+                headers: {
+                    Authorization: token
+                }
+            })
+            console.log(response.data)
+            setIsModalOpen(false)
+        } catch (error) {
+            console.error('Erro ao enviar a atualização do funcionário', error)
+        }
+    }
+
     return (
         <div className={styles.headerDivGrande}>
-
             {isModalOpen && (
-                <div
-                    className={styles.firstIsModalOpen}
-                    onClick={closeModal}
-                />
+                <>
+                    <div
+                        className={styles.firstIsModalOpen}
+                        onClick={() => { setIsModalOpen(false) }}
+                    />
+                    <div className={styles.secondIsModalOpen}>
+                        <div className={styles.divImagemModal}>
+                            <div className={styles.imagemModal}>
+                                <img src={userInfo.imgPerfil} style={{ height: '100%', width: '100%', borderRadius: '100%' }} alt="" />
+                                <button className={styles.buttonDentroImagemModal} onClick={() => { setIsModalOpen(false) }}>
+                                    <img src={editFoto} style={{ height: '50px', width: '50px' }} alt="Editar Imagem" />
+                                </button>
+                            </div>
+                        </div>
+                        <div className={styles.divTodoInputs}>
+                            <div className={styles.divEsquerdaInputs}>
+                                <TextField
+                                    label='Nome'
+                                    placeholder='Digite Aqui'
+                                    value={values.nome}
+                                    onChange={handleChange('nome')}
+                                />
+                                <TextField
+                                    label='Email'
+                                    placeholder='Digite Aqui'
+                                    value={values.email}
+                                    onChange={handleChange('email')}
+                                />
+                                <TextField
+                                    label='Senha'
+                                    placeholder='Digite Aqui'
+                                    type='password'
+                                    value={values.senha}
+                                    onChange={handleChange('senha')}
+                                />
+                                <TextField
+                                    label='Celular'
+                                    placeholder='Digite Aqui'
+                                    value={values.celular}
+                                    onChange={handleChange('celular')}
+                                />
+                            </div>
+                            <div className={styles.divDireitaInputs}>
+                                <TextField
+                                    label='Logradouro'
+                                    placeholder='Digite Aqui'
+                                    value={values.logradouro}
+                                    onChange={handleChange('logradouro')}
+                                />
+                                <TextField
+                                    label='Número'
+                                    placeholder='Digite Aqui'
+                                    value={values.numero}
+                                    onChange={handleChange('numero')}
+                                />
+                                <TextField
+                                    label='Cidade'
+                                    placeholder='Digite Aqui'
+                                    value={values.cidade}
+                                    onChange={handleChange('cidade')}
+                                />
+                                <TextField
+                                    label='CEP'
+                                    placeholder='Digite Aqui'
+                                    value={values.cep}
+                                    onChange={handleChange('cep')}
+                                />
+                            </div>
+                        </div>
+                        <div className={styles.divButtonDescartarESalvar}>
+                            <button className={styles.buttonDescartarInfos} onClick={resetValues}>Descartar Informações</button>
+                            <button className={styles.buttonSalvarInfos} onClick={enviarAtualizacaoUsuario}>Salvar Informações</button>
+                        </div>
+                    </div>
+                </>
             )}
-
-            {/* MODAL */}
-            {isModalOpen && (
-                <div className={styles.secondIsModalOpen}>
-
-                    {/* IMAGEM */}
-                    <div className={styles.divImagemModal}>
-                        <div className={styles.imagemModal}>
-                            <img src={exemploImg} style={{ height: '100%', width: '100%', borderRadius: '100%' }} alt="" />
-                            <button className={styles.buttonDentroImagemModal} onClick={() => { openModal() }}
-                            >
-                                <img src={editFoto} style={{ height: '50px', width: '50px' }} alt="Editar Imagem" />
-                            </button>
-                        </div>
-
-                    </div>
-
-                    {/* INPUTS */}
-
-                    <div className={styles.divTodoInputs}>
-
-                        <div className={styles.divEsquerdaInputs}>
-
-                            <TextField
-                                label='Nome'
-                                placeholder='Digite Aqui'
-                                value={values.nome}
-                                onChange={handleChange('nome')}
-                            />
-                            <TextField
-                                label='Email'
-                                placeholder='Digite Aqui'
-                                value={values.email}
-                                onChange={handleChange('email')}
-                            />
-                            <TextField
-                                label='Senha'
-                                placeholder='Digite Aqui'
-                                type='password'
-                                value={values.senha}
-                                onChange={handleChange('senha')}
-                            />
-                            <TextField
-                                label='Celular'
-                                placeholder='Digite Aqui'
-                                value={values.celular}
-                                onChange={handleChange('celular')}
-                            />
-
-                        </div>
-
-                        <div className={styles.divDireitaInputs}>
-                            <TextField
-                                label='Logradouro'
-                                placeholder='Digite Aqui'
-                                value={values.logradouro}
-                                onChange={handleChange('logradouro')}
-                            />
-                            <TextField
-                                label='Número'
-                                placeholder='Digite Aqui'
-                                value={values.numero}
-                                onChange={handleChange('numero')}
-                            />
-                            <TextField
-                                label='Cidade'
-                                placeholder='Digite Aqui'
-                                value={values.cidade}
-                                onChange={handleChange('cidade')}
-                            />
-                            <TextField
-                                label='CEP'
-                                placeholder='Digite Aqui'
-                                value={values.cep}
-                                onChange={handleChange('cep')}
-                            />
-
-                        </div>
-                    </div>
-
-                    {/* SALVAR E DESCARTAR INFORMAÇÕES */}
-
-                    <div className={styles.divButtonDescartarESalvar}>
-                        <button className={styles.buttonDescartarInfos} onClick={() => { resetValues() }}>Descartar Informações</button>
-                        <button className={styles.buttonSalvarInfos} onClick={() => { enviarAtualizacaoUsuario(values.email, values.nome, values.celular) }}>Salvar Informações
-                        </button>
-
-                    </div>
-                </div>
-            )}
-
-
-
-            {/* HEADER */}
-            {/* DIV BOTÃO DE SAIR */}
             <div className={styles.divTodoBotaoSair}>
-
                 <div className={styles.divParaOBotao}>
-
                     <button className={styles.botaoSair} onClick={paginaLogin}>
-
                         <div className={styles.divConteudoDentroBotao}>
-                            <img src={iconeSair} style={{ height: '23px' }} alt="" />
+                            <img src={iconeSair} style={{ height: '24px' }} alt="" />
                             <div className={styles.textoSair} onClick={handleLogout}>Sair</div>
                         </div>
-
                     </button>
-
                 </div>
-
             </div>
-
-            {/* DIV IMAGEM */}
             <div className={styles.divTodoImagem}>
-                {/* IMAGEM */}
                 <div className={styles.divImagem}>
-
-                    <img src={exemploImg} style={{ height: '101%', width: '100%', borderRadius: '100%' }} alt="" />
-
+                    <img src={userInfo.imgPerfil} style={{ height: '100%', width: '100%', borderRadius: '50%' }} alt="imagem-de-perfil" />
                 </div>
             </div>
-
-            {/* DIV INFORMAÇÕES */}
             <div className={styles.divTodoInformacoes}>
                 <div className={styles.divInformacoesConteudo}>
-
                     <div className={styles.divBemVindo}>
                         <div className={styles.divBemVindoConteudo}>
                             Bem-vindo,<span style={{ color: "#E3A74F" }}>&nbsp; {userInfo.nome}</span>!
                         </div>
-
                         <div className={styles.divBotaoEditar}>
-                            <button className={styles.botaoEditar} onClick={openModal}>
+                            <button className={styles.botaoEditar} onClick={() => setIsModalOpen(true)}>
                                 <img src={iconEditar} style={{ height: '90%', width: '90%' }} alt="" />
                             </button>
                         </div>
-
                     </div>
-
                     <div className={styles.divContato}>
-                        <div className={styles.divTextContato}>
-                            Contato
-                        </div>
-
+                        <div className={styles.divTextContato}>Contato</div>
                         <div className={styles.divTelefoneEEmail}>
-
-                            <div className={styles.divInfoConteudo}>
-                                {userInfo.celular}
-                            </div>
-
-                            <div className={styles.divInfoConteudo}>
-                                {userInfo.email}
-                            </div>
-
+                            <div className={styles.divInfoConteudo}>{userInfo.celular}</div>
+                            <div className={styles.divInfoConteudo}>{userInfo.email}</div>
                         </div>
-
                     </div>
-
                     <div className={styles.divEndereco}>
-                        <div className={styles.divTextEndereco}>
-                            Endereço
-                        </div>
-
-                        <div className={styles.divConteudoEndereco}>
-
-                            <div className={styles.divConteudoEnderecoTexto}>
-                                Rua Jericoacara, 147 - São Paulo - SP
-                            </div>
-
-                        </div>
-
+                        {userInfo.logradouro && userInfo.numero && (
+                            <>
+                                <div className={styles.divTextEndereco}>Endereço</div>
+                                <div className={styles.divConteudoEndereco}>
+                                    <div className={styles.divConteudoEnderecoTexto}>
+                                        {userInfo.logradouro}, {userInfo.numero}
+                                    </div>
+                                </div>
+                            </>
+                        )}
                     </div>
-
                 </div>
-
             </div>
-
-            {/* DIV SEPARAÇÃO */}
-            <div className={styles.divSeparacao}>
-
-            </div>
-
-            {/* DIV BOTÃO PRA TROCAR DE AMBIENTE */}
-
-
+            <div className={styles.divSeparacao}></div>
             {isAuth ? (
                 <div className={styles.validacaoAmbiente}>
                     <div className={styles.divTodoTrocaAmbiente}>
@@ -339,11 +288,10 @@ function HeaderUsuario(props) {
                             </button>
                         </div>
                     </div>
-
                     <div className={styles.divTodoTrocaAmbiente}>
                         <div className={styles.divConteudoTrocaAmbiente}>
                             <button className={styles.botaoTrocaAmbiente} onClick={abrirPerfilBarbearia}>
-                                {nomeBarbearia}
+                                {nomeNegocio}
                             </button>
                         </div>
                     </div>
@@ -362,7 +310,6 @@ function HeaderUsuario(props) {
                                     borderRadius: '0 0 10px 10px',
                                     width: '100%',
                                     height: '100%'
-
                                 }}>
                                 Possui barbearia?
                             </Button>
@@ -374,4 +321,4 @@ function HeaderUsuario(props) {
     )
 }
 
-export default HeaderUsuario;
+export default HeaderUsuario
