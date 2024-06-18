6d181642f141a210062a21f3b1e1c40f78c7d97b
Gabriella <gabriellaperniconi@gmail.com>
2024-06-17 22:00:07 -0300

    Aplicando tema

--- frontend/src/components/CardHorario/CardHorario.jsx
+++ frontend/src/components/CardHorario/CardHorario.jsx
@@ -1,41 +1,49 @@
-import { Typography } from '@mui/material'
+import { Button, ThemeProvider, Typography } from '@mui/material'
+import { theme } from '../../theme'
 
 export function CardHorario(props) {
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'row',
-      padding: 32,
-      backgroundColor: '#F4F3EE',
-      height: '70%',
-      minWidth: 300,
-      borderRadius: 16,
-      alignItems: 'center',
-      gap: 16
-    }}>
+    <ThemeProvider theme={theme}>
       <div style={{
-        height: '80%',
-        width: 2,
-        backgroundColor: '#E3A74F',
-      }} />
+        display: 'flex',
+        flexDirection: 'row',
+        padding: 32,
+        backgroundColor: '#F4F3EE',
+        height: '70%',
+        minWidth: 300,
+        borderRadius: 16,
+        alignItems: 'center',
+        gap: 16
+      }}>
+        <div style={{
+          height: '80%',
+          width: 2,
+          backgroundColor: '#E3A74F',
+        }} />
 
-      <div style={{ display: 'flex', flexDirection: 'column', gap: 16 }}>
-        <Typography variant="h6">
-          {props.nomeCliente}
-        </Typography>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 16 }}>
+          <Typography variant="h6">
+            {props.nomeCliente}
+          </Typography>
 
-        <Typography variant="body1">
-          Horário: {props.horario}
-        </Typography>
+          <Typography variant="body1">
+            Horário: {props.horario}
+          </Typography>
 
-        <Typography variant="body1">
-          Duração: {props.duracao}
-        </Typography>
+          <Typography variant="body1">
+            Duração: {props.duracao}
+          </Typography>
 
-        <Typography variant="body1">
-          Serviço: {props.servico}
-        </Typography>
+          <Typography variant="body1">
+            Serviço: {props.servico}
+          </Typography>
+
+          <Button variant='contained'
+          onClick={props.onClick}>
+            Visualizar
+          </Button>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   )
 }
