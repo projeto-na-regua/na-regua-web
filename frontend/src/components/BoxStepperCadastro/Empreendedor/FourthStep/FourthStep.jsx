5bf4d79e7c863b6709d778539c57b1bb7e5cf569
Gabriella Roman <gabriellaperniconi@gmail.com>
2024-12-01 15:23:41 -0300

    add atualizoes

--- frontend/src/components/BoxStepperCadastro/Empreendedor/FourthStep/FourthStep.jsx
+++ frontend/src/components/BoxStepperCadastro/Empreendedor/FourthStep/FourthStep.jsx
@@ -19,8 +19,7 @@ export function FourthStep() {
     cidade: '',
     estado: '',
     complemento: '',
-    nomeBarbearia: '',
-    descricao: ''
+    nomeDoNegocio: ''
   }
 
   const validationSchema = yup.object().shape({
@@ -34,7 +33,7 @@ export function FourthStep() {
   })
 
   const onSubmit = async (values) => {
-    const { cep, cpf, logradouro, numero, cidade, estado, complemento, nomeBarbearia, descricao } = values
+    const { cep, cpf, logradouro, numero, cidade, estado, complemento, nomeDoNegocio } = values
     const token = JSON.parse(sessionStorage.getItem('user'))
 
     const formData = new FormData()
@@ -47,23 +46,32 @@ export function FourthStep() {
       cidade,
       estado,
       complemento,
-      nomeBarbearia,
-      descricao
+      nomeDoNegocio
     }
 
-    formData.append('barbearia', JSON.stringify(barbeariaData))
-    formData.append('perfil', profileImage)
-    formData.append('banner', bannerImage)
+    formData.append('barbearia', barbeariaData)
+
+    if (profileImage) {
+      formData.append('perfil', profileImage)
+    } else {
+      console.warn('Imagem de perfil não selecionada.')
+    }
+
+    if (bannerImage) {
+      formData.append('banner', bannerImage)
+    } else {
+      console.warn('Imagem de banner não selecionada.')
+    }
 
     console.log('Formulário de cadastro de barbearia:', formData)
 
     try {
-      const response = await api.post('/usuarios/cadastro-barbearia', formData,
-        {
-          headers: {
-            'Authorization': token
-          }
-        })
+      const response = await api.post('/usuarios/cadastro-barbearia', formData, {
+        headers: {
+          'Authorization': token,
+          'Content-Type': 'multipart/form-data'
+        }
+      })
 
       const barbearia = response.data
       console.log('Barbearia cadastrada:', barbearia)
@@ -72,6 +80,7 @@ export function FourthStep() {
       toast.error('Erro ao cadastrar barbearia!')
     }
   }
+
 
   const fetchCep = async (cep, setFieldValue) => {
     console.log('Buscando CEP:', cep) // Log do CEP
@@ -109,20 +118,22 @@ export function FourthStep() {
   const handleImageProfileUpload = (event) => {
     const file = event.target.files[0]
     if (file) {
-      sessionStorage.setItem('imgPerfil', file)
       setProfileImage(file)
+      console.log('Arquivo de imagem de perfil:', file)
+    } else {
+      console.warn('Nenhum arquivo de imagem de perfil selecionado!')
     }
   }
-
 
   const handleImageBannerUpload = (event) => {
     const file = event.target.files[0]
     if (file) {
-      sessionStorage.setItem('imgBanner', file)
       setBannerImage(file)
+      console.log('Arquivo de imagem de banner:', file)
+    } else {
+      console.warn('Nenhum arquivo de imagem de banner selecionado!')
     }
   }
-
 
   return (
     <Box style={{ marginLeft: 32, marginRight: 32 }}>
@@ -160,7 +171,7 @@ export function FourthStep() {
                 <div style={{ display: 'flex', flexDirection: 'row', gap: 16, alignItems: 'center', marginTop: 16 }}>
                   <img
                     alt='logo da barbearia'
-                    src={profileImage || 'https://via.placeholder.com/100'}
+                    src={profileImage ? URL.createObjectURL(profileImage) : 'https://via.placeholder.com/100'}
                     style={{ width: 100, height: 100, border: '1px solid #ccc', borderRadius: '50%' }}
                   />
 
@@ -196,7 +207,7 @@ export function FourthStep() {
                 <div style={{ display: 'flex', flexDirection: 'row', gap: 16, alignItems: 'center', marginTop: 16 }}>
                   <img
                     alt='capa da barbearia'
-                    src={bannerImage || 'https://via.placeholder.com/300'}
+                    src={bannerImage ? URL.createObjectURL(bannerImage) : 'https://via.placeholder.com/300'}
                     style={{ width: 300, height: 150, border: '1px solid #ccc', borderRadius: 12 }}
                   />
 
@@ -230,33 +241,17 @@ export function FourthStep() {
                 INFORMAÇÕES DO NEGÓCIO
               </Typography>
 
-              <Typography variant='body1' style={{ color: '#9E9E9E', fontStyle: 'italic' }}>
-                Descreva sua barbearia
-              </Typography>
-
               <div style={{ display: 'flex', flexDirection: 'row', gap: 16 }}>
                 <TextField
                   type='text'
-                  name='nomeBarbearia'
-                  value={formik.values.nomeBarbearia}
+                  name='nomeDoNegocio'
+                  value={formik.values.nomeDoNegocio}
                   placeholder='Nome da Barbearia'
                   label='Nome da Barbearia'
                   onChange={formik.handleChange}
                   onBlur={formik.handleBlur}
-                  error={formik.touched.nomeBarbearia && Boolean(formik.errors.nomeBarbearia)}
-                  helperText={formik.touched.nomeBarbearia ? formik.errors.nomeBarbearia : ''}
-                />
-
-                <TextField
-                  type='text'
-                  name='descricao'
-                  value={formik.values.descricao}
-                  placeholder='Descrição'
-                  label='Descrição'
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  error={formik.touched.descricao && Boolean(formik.errors.descricao)}
-                  helperText={formik.touched.descricao ? formik.errors.descricao : ''}
+                  error={formik.touched.nomeDoNegocio && Boolean(formik.errors.nomeDoNegocio)}
+                  helperText={formik.touched.nomeDoNegocio ? formik.errors.nomeDoNegocio : ''}
                 />
               </div>
 
@@ -307,18 +302,6 @@ export function FourthStep() {
 
               <TextField
                 type='text'
-                name='complemento'
-                value={formik.values.complemento}
-                placeholder='Complemento'
-                label='Complemento'
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                error={formik.touched.complemento && Boolean(formik.errors.complemento)}
-                helperText={formik.touched.complemento ? formik.errors.complemento : ''}
-              />
-
-              <TextField
-                type='text'
                 name='cidade'
                 value={formik.values.cidade}
                 placeholder='Cidade'
@@ -341,8 +324,25 @@ export function FourthStep() {
                 helperText={formik.touched.estado ? formik.errors.estado : ''}
               />
 
-              <Button type='submit' variant='contained' color='primary'>
-                Enviar
+              <TextField
+                type='text'
+                name='complemento'
+                value={formik.values.complemento}
+                placeholder='Complemento'
+                label='Complemento'
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.complemento && Boolean(formik.errors.complemento)}
+                helperText={formik.touched.complemento ? formik.errors.complemento : ''}
+              />
+
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                style={{ marginTop: 16 }}
+              >
+                Finalizar Cadastro
               </Button>
             </Box>
           </Form>
